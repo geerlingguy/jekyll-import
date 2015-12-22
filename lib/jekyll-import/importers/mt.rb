45c27df2f1287c26131674598219e3a54dfce313
Parker Moore <parkrmoore@gmail.com>
2015-12-21 20:00:16 -0500

    MT: allow use of SQLite for installation.
    Fixes #223

--- lib/jekyll-import/importers/mt.rb
+++ lib/jekyll-import/importers/mt.rb
@@ -2,6 +2,8 @@ module JekyllImport
   module Importers
     class MT < Importer
 
+      SUPPORTED_ENGINES = %{mysql postgres sqlite}
+
       STATUS_DRAFT = 1
       STATUS_PUBLISHED = 2
       MORE_CONTENT_SEPARATOR = '<!--more-->'
@@ -65,27 +67,10 @@ module JekyllImport
       def self.process(options)
         options  = default_options.merge(options)
 
-        engine   = options.fetch('engine', 'mysql')
-        dbname   = options.fetch('dbname')
-        user     = options.fetch('user')
-        pass     = options.fetch('password', "")
-        host     = options.fetch('host', "localhost")
         comments = options.fetch('comments')
+        posts_name_by_id = {} if comments
 
-        posts_name_by_id = { } if comments
-
-        db_opts = { user => user, :password => pass, :host => host }
-
-        # Use the default port by default, but set the port if one is provided.
-        if options['port']
-          db_opts[:port] = options['port']
-        end
-
-        if engine != 'mysql' && engine != 'postgres'
-          abort("unsupported --engine value supplied '#{engine}'. Only 'mysql' and 'postgres' are supported")
-        end 
-
-        db = Sequel.public_send(engine, dbname, db_opts)
+        db = database_from_opts(opts)
 
         post_categories = db[:mt_placement].join(:mt_category, :category_id => :placement_category_id)
 
@@ -241,6 +226,34 @@ module JekyllImport
         else
           # Other values might need custom work.
           entry_type
+        end
+      end
+
+      def self.database_from_opts(opts)
+        engine   = options.fetch('engine', 'mysql')
+        dbname   = options.fetch('dbname')
+        user     = options.fetch('user')
+        pass     = options.fetch('password', "")
+        host     = options.fetch('host', "localhost")
+
+        db_connect_opts = {
+          :user => user,
+          :password => pass,
+          :host => host
+        }
+        db_connect_opts = opts['port'] if opts['port']
+
+        case engine
+        when "sqlite"
+          Sequel.sqlite(dbname)
+        when "mysql", "postgres"
+          Sequel.public_send(
+            engine,
+            dbname,
+            db_connect_opts
+          )
+        else
+          abort("Unsupported engine: '#{engine}'. Must be one of #{SUPPORTED_ENGINES.join(', ')}")
         end
       end
     end
